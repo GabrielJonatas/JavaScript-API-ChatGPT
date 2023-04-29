7a18c60b11dac1e64d67e9efc84644b4710f0b22
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2023-04-29 02:03:43 -0300

    adicionado comentários

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ config()
 import { Configuration, OpenAIApi } from "openai"
 import readline from "readline"
 
+// habilitar a api através da key
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 }))
 
+// habilitar o prompt no node
 const userInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
+// entrada de dados
 userInterface.prompt();
+
+// promise que aguarda o prompt
 userInterface.on("line", async input => {
     const res = await openai
+
+    // utiliza o chat GPT
     .createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: input}]
@@ -27,6 +34,8 @@ userInterface.on("line", async input => {
 
 /*userInterface.on("line", async input => {
     const res = await openai
+
+    // Somente responde perguntas
     .createCompletion({
         model: "text-davinci-003",
         prompt: input,
